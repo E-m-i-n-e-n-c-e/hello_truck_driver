6c3e833706bf4c40ef0b9906cb89dc04c163d694
E-m-i-n-e-n-c-e <kssakhilraj@gmail.com>
2025-12-18 19:11:31 +0530

    Optional dummy data

--- lib/providers/assignment_providers.dart
+++ lib/providers/assignment_providers.dart
@@ -10,14 +10,14 @@ import 'package:hello_truck_driver/utils/mock_data.dart';
 /// Current assignment for the driver (can be null)
 final currentAssignmentProvider = FutureProvider.autoDispose<BookingAssignment?>((ref) async {
   final API api = await ref.read(apiProvider.future);
-  BookingAssignment? assignment = await assignment_api.getCurrentAssignment(api);
-  return assignment ?? MockData.getMockCurrentAssignment();
+  final assignment = await assignment_api.getCurrentAssignment(api);
+  return assignment;
 });
 
 /// Assignment history for the driver
 final assignmentHistoryProvider = FutureProvider.autoDispose<List<BookingAssignment>>((ref) async {
   final API api = await ref.read(apiProvider.future);
-  List<BookingAssignment> history = await assignment_api.getAssignmentHistory(api);
+  final history = await assignment_api.getAssignmentHistory(api);
   return history.isNotEmpty ? history : MockData.getMockAssignmentHistory();
 });
 
--- lib/providers/payment_providers.dart
+++ lib/providers/payment_providers.dart
@@ -1,26 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hello_truck_driver/api/earnings_api.dart' as dashboard_api;
 import 'package:hello_truck_driver/models/transaction_log.dart';
 import 'package:hello_truck_driver/models/wallet_log.dart';
+import 'package:hello_truck_driver/providers/auth_providers.dart';
 import 'package:hello_truck_driver/utils/mock_data.dart';
 
 /// Wallet logs (latest 50)
 final walletLogsProvider = FutureProvider.autoDispose<List<WalletLog>>((ref) async {
-  // Simulate network delay
-  await Future.delayed(const Duration(milliseconds: 600));
-  return MockData.getMockWalletLogs();
-
-  // TODO: Uncomment when backend is ready
-  // final api = await ref.watch(apiProvider.future);
-  // return dashboard_api.getWalletLogs(api);
+  final api = await ref.watch(apiProvider.future);
+  final logs = await dashboard_api.getWalletLogs(api);
+  return logs.isEmpty ? MockData.getMockWalletLogs() : logs;
 });
 
 /// Transaction logs (latest 50)
 final transactionLogsProvider = FutureProvider.autoDispose<List<TransactionLog>>((ref) async {
-  // Simulate network delay
-  await Future.delayed(const Duration(milliseconds: 700));
-  return MockData.getMockTransactionLogs();
-
-  // TODO: Uncomment when backend is ready
-  // final api = await ref.watch(apiProvider.future);
-  // return dashboard_api.getTransactionLogs(api);
+  final api = await ref.watch(apiProvider.future);
+  final logs = await dashboard_api.getTransactionLogs(api);
+  return logs.isEmpty ? MockData.getMockTransactionLogs() : logs;
 });
